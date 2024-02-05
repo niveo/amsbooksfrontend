d75344b6110e92524deeae1e85f793be45191495
amsraimar <amsraimar@gmail.com>
2024-02-05 15:42:57 -0300

    Passando versão do sistema para footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
+import version from "./version";
+
 export const environment = {
   production: true,
   apiUri: 'https://amsbooksbackend.onrender.com', 
-  carregarImagemRemota: true
+  carregarImagemRemota: true,
+  ...version
 };
