ea0cb6771849f80a858cbdb98fab0832813b32f7
amsraimar <amsraimar@gmail.com>
2024-02-06 16:15:18 -0300

    Pegando dados do usuário pela sessão

--- src/app/stores/usuario-perfil.store.ts
+++ src/app/stores/usuario-perfil.store.ts
@@ -1,11 +1,16 @@
 import { AutenticacaoStore } from 'src/app/stores';
 import { Injectable, inject } from '@angular/core';
-import { fetchUserAttributes } from 'aws-amplify/auth';
-import { BehaviorSubject, from, map } from 'rxjs';
+import { BehaviorSubject, from, map, mergeMap } from 'rxjs';
 import { ImagemRemotaService, UsuarioPerfilRemotoService } from '../services';
-import { DIRETORIO_IMAGEM_USUARIO } from '../common';
+import {
+  CognitoIdentityServiceProvider,
+  DIRETORIO_IMAGEM_USUARIO,
+  TOKEN_AWS_AUTH,
+} from '../common';
 import { NzUploadFile } from 'ng-zorro-antd/upload';
 import { PerfilUsuario } from '../model';
+import { sessionStorage } from 'aws-amplify/utils';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -23,6 +28,8 @@ export class UsuarioPerfilStore {
   );
   private readonly autenticacaoStore = inject(AutenticacaoStore);
 
+  private readonly awsAuthConfig = inject(TOKEN_AWS_AUTH);
+
   constructor() {
     this.autenticacaoStore.usuarioLogado$.subscribe((logado) => {
       if (logado) {
@@ -32,17 +39,37 @@ export class UsuarioPerfilStore {
   }
 
   private atualizarPerfilUsuario() {
-    from(fetchUserAttributes()).subscribe({
-      next: (value) => {
-        value['picture'] = this._imagemRemotaService.getUrlPublic(
-          DIRETORIO_IMAGEM_USUARIO + value.sub
-        );
-        this._usuarioPerfilSource.next(value as PerfilUsuario);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    from(
+      sessionStorage.getItem(
+        `${CognitoIdentityServiceProvider}.${this.awsAuthConfig.userPoolClientId}.LastAuthUser`
+      )
+    )
+      .pipe(
+        mergeMap((lastAuthUser) => {
+          return from(
+            sessionStorage.getItem(
+              `${CognitoIdentityServiceProvider}.${this.awsAuthConfig.userPoolClientId}.${lastAuthUser}.idToken`
+            )
+          );
+        }),
+        map((token: string) => {
+          return jwtDecode(token);
+        })
+      )
+      .subscribe({
+        next: (value: any) => {
+          value['picture'] = this._imagemRemotaService.getUrlPublic(
+            DIRETORIO_IMAGEM_USUARIO + value.sub
+          );
+          this._usuarioPerfilSource.next({
+            name: value.name,
+            email: value.email,
+          });
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
   alterarNome() {
@@ -55,6 +82,8 @@ export class UsuarioPerfilStore {
       .upload(DIRETORIO_IMAGEM_USUARIO + this.autenticacaoStore.getUserId, file)
       .pipe(
         map((atualizado) => {
+          console.log(atualizado);
+
           if (atualizado) {
             this.atualizarPerfilUsuario();
           }
