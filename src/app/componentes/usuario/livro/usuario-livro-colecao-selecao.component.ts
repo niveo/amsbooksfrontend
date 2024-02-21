bf56b0fea889a29838829765e2e4ce037ba74291
amsraimar <amsraimar@gmail.com>
2024-02-21 18:06:00 -0300

    colecao livro com tag

--- src/app/componentes/usuario/livro/usuario-livro-colecao-selecao.component.ts
+++ src/app/componentes/usuario/livro/usuario-livro-colecao-selecao.component.ts
@@ -1,43 +1,31 @@
-import {
-  LoadingType,
-  SelectModule,
-  LoadingModule,
-  SelectComponent,
-} from 'ng-devui';
-import { Component, Input, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ColecaoLivroVinculoService } from 'src/app/services/colecao-livro-vinculo.service';
-import { Observable, pipe, tap } from 'rxjs';
+import { pipe, tap } from 'rxjs';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { MSG_ERRO_PROCESSAR } from 'src/app/common';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 
 @Component({
   selector: 'usuario-livro-colecao-selecao-component',
   templateUrl: './usuario-livro-colecao-selecao.component.html',
-  styles: `nz-select {width: 100%}`,
   standalone: true,
-  imports: [FormsModule, LoadingModule, SelectModule],
+  imports: [FormsModule, NzTagModule],
   providers: [ColecaoLivroVinculoService],
 })
 export class UsuarioLivroColecaoSelecaoComponent implements OnInit {
   @Input({ required: true })
   livroId: number;
 
-  options = [];
-  select1 = [];
-
-  selectCache = new Set();
-
-  loading: LoadingType;
-  source = new Observable();
-
-  @ViewChild('networkSearchSelect') selectComponent: SelectComponent;
+  listOfOption: any[] = [];
+  listOfSelectedValue = [];
+  listOfSelectedValueCache = [];
 
   private readonly colecaoLivroService = inject(ColecaoLivroVinculoService);
 
   private readonly nzMessageService = inject(NzMessageService);
 
-  pipeTapError = (id: number) =>
+  private pipeTapError = (id: number) =>
     pipe(
       tap({
         error: (err) => {
@@ -46,63 +34,57 @@ export class UsuarioLivroColecaoSelecaoComponent implements OnInit {
             this.msgErroReset();
           }, 300);
         },
-        next: () => {
-          this.selectCache.has(id)
-            ? this.selectCache.delete(id)
-            : this.selectCache.add(id);
-        },
       })
     );
 
   ngOnInit(): void {
     this.colecaoLivroService.getAll(this.livroId).subscribe({
       next: (value) => {
-        this.options = value?.map((m) => {
-          return { name: m.descricao, value: m.id };
-        });
-        this.select1 = value
+        this.listOfOption = value;
+        this.listOfSelectedValue = value
           .filter((v) => v.vinculado === 1)
-          ?.map((m) => {
-            this.selectCache.add(m.id);
-
-            return { name: m.descricao, value: m.id };
-          });
+          ?.map((m) => m.id);
+        this.listOfSelectedValueCache = [...this.listOfSelectedValue];
       },
     });
   }
 
-  change(e: any) {
-    if (!this.selectCache.has(e.value)) {
-      this.loading = this.colecaoLivroService
+  checkChange(checked, item) {
+    this.change(checked, item.id);
+  }
+
+  private change(checked: boolean, colecaoId: number) {
+    if (checked) {
+      this.colecaoLivroService
         .create({
-          colecaoId: e.value,
+          colecaoId: colecaoId,
           livroId: this.livroId,
         })
-        .pipe(this.pipeTapError(e.id))
+        .pipe(this.pipeTapError(colecaoId))
         .subscribe({
           error: () => {
-            const index = this.select1.findIndex((i) => i.value === e.value);
-            this.select1.splice(index, 1);
+            const index = this.listOfSelectedValue.findIndex(
+              (i) => i.value === colecaoId
+            );
+            this.listOfSelectedValue.splice(index, 1);
           },
         });
     } else {
-      this.loading = this.colecaoLivroService
+      this.colecaoLivroService
         .delete({
-          colecaoId: e.value,
+          colecaoId: colecaoId,
           livroId: this.livroId,
         })
-        .pipe(this.pipeTapError(e.id))
+        .pipe(this.pipeTapError(colecaoId))
         .subscribe({
           error: () => {
-            this.select1.push(e);
+            this.listOfSelectedValue.push(colecaoId);
           },
         });
     }
   }
 
   msgErroReset() {
-    this.selectComponent.resetStatus();
-    this.selectComponent.resetSource();
     this.nzMessageService.error(MSG_ERRO_PROCESSAR);
   }
 }
