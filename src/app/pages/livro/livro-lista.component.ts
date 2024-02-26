998061cdfd777daa8081041874f65add96a8f759
amsraimar <amsraimar@gmail.com>
2024-02-26 17:41:13 -0300

    Carregar livros por Observable

--- src/app/pages/livro/livro-lista.component.ts
+++ src/app/pages/livro/livro-lista.component.ts
@@ -2,9 +2,9 @@ import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { finalize } from 'rxjs';
+import { Observable, finalize, from, map, mergeMap, tap } from 'rxjs';
+import { MSG_ERRO_CARREGAR } from 'src/app/common';
 import { LivroService } from 'src/app/services/livro.service';
-import { MonitorErroStore } from 'src/app/stores';
 
 @Component({
   selector: 'app-livro-lista-component',
@@ -15,14 +15,14 @@ export class LivroListaComponent implements OnInit {
   private readonly router = inject(Router);
   private readonly route = inject(ActivatedRoute);
   private readonly livroService = inject(LivroService);
-  private readonly monitorErroStore = inject(MonitorErroStore);
   protected readonly destroyRef = inject(DestroyRef);
-  livros: any[] = [];
+  private readonly nzMessageService = inject(NzMessageService);
+
   count: number;
   page: number = 0;
   pageSize: number = 10;
-
   loading = true;
+  registros$!: Observable<any[]>;
 
   get pageLimit() {
     return Math.round(this.count / this.pageSize);
@@ -34,28 +34,41 @@ export class LivroListaComponent implements OnInit {
 
   private carregarRegistros() {
     this.loading = true;
-    this.livros = [];
-    this.route.paramMap
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((params) => {
-        const obs = {};
-        params.keys.forEach((key) => {
-          obs[key] = params.get(key);
-        });
-        this.livroService
-          .getAll(this.pageSize, this.page, obs)
-          .pipe(finalize(() => (this.loading = false)))
-          .pipe(takeUntilDestroyed(this.destroyRef))
-          .subscribe({
-            next: (response) => {
-              this.livros = response.results;
-              this.count = response.count;
-            },
-            error: (error) => {
-              this.monitorErroStore.notificar(error);
-            },
+    this.registros$ = this.route.paramMap
+      .pipe(
+        map((params) => {
+          const obs = {};
+          params.keys.forEach((key) => {
+            obs[key] = params.get(key);
           });
-      });
+          return obs;
+        }),
+        mergeMap((params) => {
+          return from(
+            this.livroService.getAll(this.pageSize, this.page, params).pipe(
+              finalize(() => (this.loading = false)),
+              takeUntilDestroyed(this.destroyRef)
+            )
+          );
+        }),
+
+        map((data) => {
+          this.count = data.count;
+          return data.results;
+        })
+      )
+
+      .pipe(
+        tap({
+          error: (err) => {
+            console.error('error', err);
+            setTimeout(() => {
+              this.nzMessageService.error(MSG_ERRO_CARREGAR);
+            }, 300);
+          },
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      );
   }
 
   detalharLivro(livro) {
