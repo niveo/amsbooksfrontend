027000832a242a1d541410ddf8dca28cc658495a
amsraimar <amsraimar@gmail.com>
2024-02-27 10:42:18 -0300

    Teste amplify conf producao

--- amplify.js
+++ amplify.js
@@ -1,3 +1,3 @@
 const { writeFileSync  } = require('fs');
-writeFileSync('dist/amplifyconfiguration.json', process.env[ 'AMPLIFY_CONFIGURATION' ]);
+writeFileSync('dist/amplifyconfiguration.json', process.env[ 'AMPLIFY_CONFIGURATION']);
 
